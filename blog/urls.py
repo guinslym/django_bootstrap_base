73c83811b875b9d0127b0af14e8f9ffb44119908
Guinsly Mondésir <guinslym@gmail.com>
2016-11-07 02:52:00 -0500

    new gitignore

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
+from .views import HomeView
+
 urlpatterns = [
- 	url(r'^$', TemplateView.as_view(template_name="home.html")),
+ 	url(r'^$', HomeView.as_view() ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+# some_app/views.py
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = "home.html"
